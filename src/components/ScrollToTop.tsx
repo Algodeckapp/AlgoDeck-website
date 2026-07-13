6c8c120c1ffd213de089bc894119311d81154c11
Christopher <christopherebisine2009@gmail.com>
2026-07-13 12:40:13 -0700

    fix: resolve SEO canonical tags & fix type checking errors

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -6,6 +6,18 @@ export default function ScrollToTop() {
 
   useEffect(() => {
     window.scrollTo(0, 0)
+
+    // Update canonical link dynamically
+    let link: HTMLLinkElement | null = document.querySelector("link[rel='canonical']")
+    const canonicalUrl = `https://algodeck.app${pathname === '/' ? '' : pathname}`
+    if (link) {
+      link.setAttribute('href', canonicalUrl)
+    } else {
+      link = document.createElement('link')
+      link.setAttribute('rel', 'canonical')
+      link.setAttribute('href', canonicalUrl)
+      document.head.appendChild(link)
+    }
   }, [pathname])
 
   return null
